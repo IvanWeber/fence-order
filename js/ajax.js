e15637e9bfc1659c940f527073617537b3b491ae
Ivan Weber <ivanbdn@yandex.ru>
2020-11-16 19:15:30 +0300

    close button fix

--- js/ajax.js
+++ js/ajax.js
@@ -6,6 +6,13 @@ $("#ajax_form").submit(function(e){
         data: $("#ajax_form").serialize(),
         success: function(data) {
             $("#ajax-result-form").html(data);
+
+            const closeButton = document.querySelector('.ajax-result-form__close-button');
+            const ajaxResultSection = document.querySelector('#ajax-result-form');
+            const clickCloseButtonHandler = () => {
+                ajaxResultSection.style.display = 'none';
+            }
+            closeButton.addEventListener('click', clickCloseButtonHandler);
         }
     });
 });
--- php/ajax-send-form.php
+++ php/ajax-send-form.php
@@ -1,7 +1,7 @@
 <?php 
 
 if(mail('ivanbdn@yandex.ru','тестовое задание, заказ забора 1275', $_POST['name-input'] . ', заказ 1275 сформирован. В ближайшее время наш специалист свяжется с вами по телефону ' . $_POST['phone-input'] . '.', 'Content-type: text/html; charset=utf-8')){
-    echo '<p class="ajax-result-par-1">' . $_POST['name-input']  . '<br> заказ № 1275 сформирован!</p><p class="ajax-result-par-2">Мы повторили его комплектацию на почту<br> ' . $_POST['email-input']  . ' </p><p class="ajax-result-par-3">В ближайшее время наш специалист<br> свяжется с вами по телефону<br> ' . $_POST['phone-input']  . '</p>';
+    echo '<button class="ajax-result-form__close-button">Закрыть</button><p class="ajax-result-par-1">' . $_POST['name-input']  . '<br> заказ № 1275 сформирован!</p><p class="ajax-result-par-2">Мы повторили его комплектацию на почту<br> ' . $_POST['email-input']  . ' </p><p class="ajax-result-par-3">В ближайшее время наш специалист<br> свяжется с вами по телефону<br> ' . $_POST['phone-input']  . '</p>';
 }
 else{
     echo 'Ошибка отправки сообщения.';
